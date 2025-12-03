b40239c881352c71dd10cd7b58c81ac9a8b09e8a
federicosimone <federico.simone15@gmail.com>
2025-12-03 23:11:42 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   return (
     <>
       <div>
-        <h1 class="red-text">Vite + React = Hello World!</h1>
+        <h1 class="">Vite + React = Hello World!</h1>
         <p>"La felicità si può trovare anche negli attimi più tenebrosi, se solo uno si ricorda di accendere la luce."</p>
+        <img src="https://ilragazzosopravvissuto.wordpress.com/wp-content/uploads/2016/06/silente.jpg?w=355&h=266" alt="Dumbledore" />
       </div>
     </>
   )
